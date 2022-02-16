a9dc9f385081e5c0a8587a62dc6375419ad62075
Andrew Berry <andrewb@users.noreply.github.com>
2022-02-16 13:08:40 -0500

    update remaining paths

--- test/createManager.spec.js
+++ test/createManager.spec.js
@@ -113,7 +113,7 @@ describe("createManager", () => {
     it("uses gpt when already exists", done => {
         window.googletag = googletag;
         adManager
-            .load("//www.googletagservices.com/tag/js/gpt-invalid.js")
+            .load("//securepubads.g.doubleclick.net/tag/js/gpt-invalid.js")
             .then(() => {
                 expect(adManager.isLoaded).to.be.true;
                 done();
@@ -132,7 +132,7 @@ describe("createManager", () => {
     it("handles invalid url", done => {
         adManager = createManager();
         adManager
-            .load("//www.googletagservices.com/tag/js/gpt-invalid.js")
+            .load("//securepubads.g.doubleclick.net/tag/js/gpt-invalid.js")
             .catch(err => {
                 expect(err.message).to.equal("failed to load script");
                 done();
