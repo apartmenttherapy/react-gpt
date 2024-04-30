bee5a892fb3a5f913c932b8a5246140389a8828b
Andrew Sotiriou <andrew.sotiriou@apartmenttherapy.com>
2024-04-30 11:39:53 -0400

    Quick fix

--- src/Bling.js
+++ src/Bling.js
@@ -176,10 +176,6 @@ class Bling extends React.Component {
          */
         viewableThreshold: PropTypes.number,
         /**
-         * Skip isInViewport check if true.
-         */
-        doNotUseViewableThreshold: PropTypes.bool,
-        /**
          * An optional call back function to notify when the script is loaded.
          *
          * @property onScriptLoaded
@@ -262,10 +258,6 @@ class Bling extends React.Component {
          * An optional flag to indicate whether an ad should only render when it's fully in the viewport area. Default is `true`.
          */
         renderWhenViewable: true,
-        /**
-         * If true skip isInViewport check.
-         */
-        doNotUseViewableThreshold: true,
         /**
          * An optional number to indicate how much percentage of an ad area needs to be in a viewable area before rendering. Default value is 0.5.
          * Acceptable range is between 0 and 1.
@@ -664,10 +656,7 @@ class Bling extends React.Component {
 
     notInViewport(props = this.props, state = this.state) {
         const {inViewport} = state;
-        return (
-            this.getRenderWhenViewable(props) &&
-            (!props.doNotUseViewableThreshold && !inViewport)
-        );
+        return this.getRenderWhenViewable(props) && (false && !inViewport);
     }
 
     defineSlot() {
