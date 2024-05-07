e3ff4767973fba877d929f0b52b6a77f6c3879df
Andrew Sotiriou <andrew.sotiriou@apartmenttherapy.com>
2024-05-07 13:45:07 -0400

    remove test mode

--- src/Bling.js
+++ src/Bling.js
@@ -4,8 +4,9 @@ import PropTypes from "prop-types";
 import {debounce} from "throttle-debounce";
 import invariant from "invariant";
 import deepEqual from "deep-equal";
+import hoistStatics from "hoist-non-react-statics";
 import {Events, filterProps} from "./oneFile";
-import {createManager} from "./createManager";
+import {createManager, pubadsAPI} from "./createManager";
 /**
  * An Ad Component using Google Publisher Tags.
  * This component should work standalone w/o context.
@@ -853,3 +854,12 @@ class Bling extends React.Component {
         });
     }
 }
+
+// proxy pubads API through Bling
+export default hoistStatics(
+    Bling,
+    pubadsAPI.reduce((api, method) => {
+        api[method] = (...args) => Bling._adManager.pubadsProxy({method, args});
+        return api;
+    }, {})
+);
